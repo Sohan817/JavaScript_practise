22135db6e6329011ea2911dc23186060eb3dc1f1
Md.Shohan Hossain <52854076+Sohan817@users.noreply.github.com>
2023-07-10 04:03:36 +0600

    Timing Events

---
+++ Lesson_58/timing_event.js
@@ -0,0 +1,26 @@
+//timing event
+//setTimeout(),setInterval()
+// setTimeout(display,4000);
+// function display(){
+//     document.write("Hello Sohan");
+// }
+var but = document.querySelector(".save-btn");
+var par = document.querySelector(".message");
+// but.addEventListener("click",saveUser);
+
+// function saveUser(){
+//     par.textContent = "User Registration Successful";
+//     setTimeout(()=>{
+//         par.textContent = "";
+//     },3000);
+// }
+// but.addEventListener("click",Counter);
+// function Counter(){
+//     var c = 100;
+//     par.textContent = c;
+//     setInterval(() => {
+//         c--;
+//         par.textContent = c;
+
+//     }, 1000);
+// }
