95765a5542cbae5d4b5e5a6e3efc859b298bf05d
Md.Shohan Hossain <52854076+Sohan817@users.noreply.github.com>
2023-07-11 04:06:05 +0600

    String method ( startWith(),endWith(),includes() )

---
+++ Lesson_76/String_method.js
@@ -0,0 +1,10 @@
+//startsWith()
+
+const message = "Today is Friday";
+console.log(message.startsWith("Today",0));
+
+//endsWith()
+console.log(message.endsWith("Friday"));
+
+//includes()
+console.log(message.includes("is"));
