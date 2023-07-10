530323a8650afe39c8f07309642dd60e790f9255
Md.Shohan Hossain <52854076+Sohan817@users.noreply.github.com>
2023-07-11 04:02:45 +0600

    Arrow Function(ES6)

---
+++ Lesson_72/arrow_function.js
@@ -0,0 +1,21 @@
+// function display1(){ console.log("Treditional Function");}
+// const display2 = () =>  console.log("Arrow Function");
+// display1();
+// display2();
+// function message1(){
+//      return "Treditional Function";
+// }
+// console.log(message1());
+
+// const message2 = () => "Arrow Function";
+// console.log(message2());
+
+function add1(n1,n2){
+    return n1+n2;
+}
+console.log(add1(10,20));
+
+const add2 = (n1,n2) =>
+    n1+n2;
+
+console.log(add2(20,20));
