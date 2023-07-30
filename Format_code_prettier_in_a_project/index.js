87b8ea3cf97dcf2f6834cbddc15fdd0fe1e5d000
Md.Shohan Hossain <52854076+Sohan817@users.noreply.github.com>
2023-07-31 03:44:02 +0600

    Format code prettier

---
+++ Format_code_prettier_in_a_project/index.js
@@ -0,0 +1,18 @@
+const firstName = 'Sohan';
+
+const lastName = 'Nadim';
+
+const fullName = firstName + lastName;
+
+console.log('Hello');
+
+if (firstName == 'Sohan') {
+  console.log('true');
+}
+const user = {
+  name: 'Shorna',
+  id: 123
+};
+const printDetain = (name) => {
+  console.log(name);
+};
